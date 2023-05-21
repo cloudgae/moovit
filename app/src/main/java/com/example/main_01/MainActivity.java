29509d6ba4ab5a9fd35f8bcd64eab22a1c3aefcb
jeongdajeong <jisu5868@gmail.com>
2023-05-22 00:32:16 +0900

    피그마 상 1~6 연결 완료 7~9 화면 작성 완(9는 수정 예정)

--- app/src/main/java/com/example/main_01/MainActivity.java
+++ app/src/main/java/com/example/main_01/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.main_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.main_01.onboarding.TypeTest_1;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button gotobtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //온보딩으로 바로 이동
+        Intent i = new Intent(MainActivity.this, TypeTest_1.class);
+        startActivity(i);
+
+
     }
 }
