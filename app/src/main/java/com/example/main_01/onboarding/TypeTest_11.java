a39aa03edba4f83a57d115576afaa58c66f6c034
irinyang <irinyang@naver.com>
2023-06-17 15:56:40 +0900

    shorts try1

--- app/src/main/java/com/example/main_01/onboarding/TypeTest_11.java
+++ app/src/main/java/com/example/main_01/onboarding/TypeTest_11.java
@@ -3,16 +3,17 @@ package com.example.main_01.onboarding;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.MediaController;
-import android.widget.VideoView;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.main_01.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -20,57 +21,62 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class TypeTest_11 extends AppCompatActivity {
+    private SimpleExoPlayer player;
+    private com.google.android.exoplayer2.ui.PlayerView playerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type_test11);
 
+        playerView = findViewById(R.id.player_view);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference docRef = db.collection("Class").document("test");
 
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                // 필드에서 영상 이름 가져오기
-                String videoName = documentSnapshot.getString("name");
+        docRef.get().addOnSuccessListener(documentSnapshot -> {
+            // 필드에서 영상 이름 가져오기
+            String videoName = documentSnapshot.getString("name");
+            // 가져온 영상 이름을 사용하여 Storage에서 참조 생성
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            StorageReference videoRef = storageRef.child("videos/" + videoName);
 
-                // Firebase Storage에서 참조 생성
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                StorageReference storageRef = storage.getReference();
-                StorageReference videoRef = storageRef.child("videos/" + videoName);
-
-                // 영상 다운로드 URL 가져오기
-                videoRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        // URL을 사용하여 영상 로드 및 표시
-                        String videoUrl = uri.toString();
-                        VideoView videoView = findViewById(R.id.video1);
-
-                        // VideoView에 MediaController 추가
-                        MediaController mediaController = new MediaController(TypeTest_11.this);
-                        videoView.setMediaController(mediaController);
-                        mediaController.setAnchorView(videoView);
-
-                        // 영상 설정
-                        videoView.setVideoURI(Uri.parse(videoUrl));
-                        videoView.start();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // 영상 URL을 가져오는 도중 에러 발생
-                        e.printStackTrace();
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                // Firestore에서 데이터 가져오는 도중 에러 발생
-                e.printStackTrace();
-            }
+            videoRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                // URL을 사용하여 영상 로드 및 표시
+                String videoUrl = uri.toString();
+                initializePlayer(videoUrl);
+            }).addOnFailureListener(e -> {
+                // 영상 URL을 가져오는 도중 에러 발생
+                // TODO: 에러 처리
+            });
+        }).addOnFailureListener(e -> {
+            // Firestore에서 데이터 가져오는 도중 에러 발생
+            // TODO: 에러 처리
         });
+    }
+
+    private void initializePlayer(String videoUrl) {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+        // 미디어 소스 생성
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, getString(R.string.app_name)));
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(videoUrl));
+
+        // 플레이어에 미디어 소스 설정
+        player.setMediaSource(mediaSource);
+        player.prepare();
+        player.setPlayWhenReady(true); // 재생 시작
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (player != null) {
+            player.release(); // 플레이어 해제
+        }
     }
 }
